78f04a27d7260132b33de4e746e9ef1e4a57f1e2
Karoly Hornyak <karoly.webdev@gmail.com>
2025-12-25 07:35:53 +0000

    Rename data.js to namedays.data.js

--- src/routes/v1/namedays.routes.js
+++ src/routes/v1/namedays.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const nameDays = require('../../data');
+const nameDays = require('../../data/namedays.data');
 
 /**
  * @swagger
